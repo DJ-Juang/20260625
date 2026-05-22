54f5d4282fdb4b291c0f4f3db9bdcb2add154fea
DJ-Juang <derjengjuang@gmail.com>
2026-05-22 16:57:51 +0800

    Update data.js

--- data.js
+++ data.js
@@ -2,6 +2,6 @@
 const rawData = [
 {id: 1, year: "2024", region: "義大利",country: "米蘭",url: "https://drive.google.com/file/d/1m_UFVphK9yd-FiRcfWvChMOm0G8tMnOK/view" , memo1:"(外海)"},
 {id: 2, year: "2024", region: "瑞士",country: "伯恩",url: "https://drive.google.com/file/d/1mOvsoJjDDRhVSEfAoHo3EX8viqonhIUn/view" , memo1:""},
-{id: 3, year: "2024", region: "瑞士",country: "伯恩",url: "https://drive.google.com/file/d/1_x03q-TwGOZPaEqmhMkzj_g-4k_IpbSE/view" , memo1:"(維爾紐斯)"},
+{id: 3, year: "2024", region: "瑞士",country: "蘇黎世",url: "https://drive.google.com/file/d/1_x03q-TwGOZPaEqmhMkzj_g-4k_IpbSE/view" , memo1:"(維爾紐斯)"},
 {id: 4, year: "2013", region: "義大利",country: "比薩",url: "https://drive.google.com/file/d/1DfKvy2Lx7zXJdL8RBCgqyLwjrR4ZrvnP/view?usp=drive_link" , memo1:""},
 ];
