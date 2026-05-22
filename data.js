a081c7a9555df47207c2f5d92d47e60326e080be
DJ-Juang <derjengjuang@gmail.com>
2026-05-22 16:27:00 +0800

    Update country names and regions in rawData

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 // data.js
 const rawData = [
-{id: 1, year: "2024", region: "義大利",country: "上海",url: "https://drive.google.com/file/d/1m_UFVphK9yd-FiRcfWvChMOm0G8tMnOK/view" , memo1:"(外灘)"},
-{id: 2, year: "2024", region: "瑞士",country: "上海",url: "https://drive.google.com/file/d/1mOvsoJjDDRhVSEfAoHo3EX8viqonhIUn/view" , memo1:""},
-{id: 3, year: "2024", region: "瑞士",country: "立陶宛",url: "https://drive.google.com/file/d/1_x03q-TwGOZPaEqmhMkzj_g-4k_IpbSE/view" , memo1:"(維爾紐斯主教座堂)"},{id: 950, year: "2013", region: "中國",country: "張家界",url: "https://drive.google.com/file/d/1XwOnm8lF2PFlAK-SYZw21q7eFkDoEUmi/view?usp=drive_link" , memo1:""},
-{id: 951, year: "2013", region: "義大利",country: "張家界",url: "https://drive.google.com/file/d/1DfKvy2Lx7zXJdL8RBCgqyLwjrR4ZrvnP/view?usp=drive_link" , memo1:""},
+{id: 1, year: "2024", region: "義大利",country: "米蘭",url: "https://drive.google.com/file/d/1m_UFVphK9yd-FiRcfWvChMOm0G8tMnOK/view" , memo1:"(外海)"},
+{id: 2, year: "2024", region: "瑞士",country: "伯恩",url: "https://drive.google.com/file/d/1mOvsoJjDDRhVSEfAoHo3EX8viqonhIUn/view" , memo1:""},
+{id: 3, year: "2024", region: "瑞士",country: "伯恩",url: "https://drive.google.com/file/d/1_x03q-TwGOZPaEqmhMkzj_g-4k_IpbSE/view" , memo1:"(維爾紐斯)"},
+{id: 4, year: "2013", region: "義大利",country: "比薩",url: "https://drive.google.com/file/d/1DfKvy2Lx7zXJdL8RBCgqyLwjrR4ZrvnP/view?usp=drive_link" , memo1:""},
 ];
