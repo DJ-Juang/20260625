5152500a6f5ce50962a8a0d0d3f4c8586b5db563
DJ-Juang <derjengjuang@gmail.com>
2026-05-23 13:06:08 +0800

    Update italy-data.js

--- italy-data.js
+++ italy-data.js
@@ -1,4 +1,4 @@
 // data.js
 const rawData = [
-{id: 1, year: "2026", region: "瑞士",country: "盧森",url: "https://drive.google.com/file/d/1m_UFVphK9yd-FiRcfWvChMOm0G8tMnOK/view" , memo1:"- 卡貝爾橋"},
+{id: 1, year: "2025", region: "瑞士",country: "盧森",url: "https://drive.google.com/file/d/1m_UFVphK9yd-FiRcfWvChMOm0G8tMnOK/view" , memo1:"- 卡貝爾橋"},
 ];
