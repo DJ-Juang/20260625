9fbe27667e209043fd799499ab9ed65e48b6c8bf
DJ-Juang <derjengjuang@gmail.com>
2026-06-20 12:17:14 +0800

    Update travel data in italy-data.js

--- italy-data.js
+++ italy-data.js
@@ -5,8 +5,8 @@ const travelData = [
 {id: 3,title: "蘇黎世", type: "image", url: "https://drive.google.com/file/d/1P_ga39KlSYABSHDyFxVW8ABHWXmO4vzF/view?usp=drive_link", category: "瑞士"},
 {id: 4,title: "熊布朗宮", type: "video", url: "https://drive.google.com/file/d/14AzRk1S8X7IzzdOje34cwuwkXwN4LgJL/view?usp=drive_link", category: "維也納"},
 {id: 0,title: "畢業了", type: "video", url: "https://drive.google.com/file/d/1UiykXqDQ9wz0UXYabzgneU5KGyykZd1P/view?usp=drive_link", category: "桃園"},
-{id: 26,title: "清水斷崖", type: "video", url: "https://drive.google.com/file/d/1DgLM2KZuNNlWEqAl7aFYTMna7Rzupzfj/view?usp=drive_link", category: "花蓮"},
-{id: 27,title: "esim", type: "video", url: "https://drive.google.com/file/d/10tsmtE2B_dXCB7LGV0GVBKSrGZu8RNNY/view?usp=drive_link", category: "瑞士"},
+{id: 5,title: "清水斷崖", type: "video", url: "https://drive.google.com/file/d/1DgLM2KZuNNlWEqAl7aFYTMna7Rzupzfj/view?usp=drive_link", category: "花蓮"},
+{id: 6,title: "esim", type: "video", url: "https://drive.google.com/file/d/10tsmtE2B_dXCB7LGV0GVBKSrGZu8RNNY/view?usp=drive_link", category: "瑞士"},
   {
     id: 5,
     title: "少女峰百年齒軌列車",
